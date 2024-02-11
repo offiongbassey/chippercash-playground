275f1260f2bb884dd0b69b352de49c8a0a127e65
Offiong Bassey <naijaspeed1@gmail.com>
2024-02-11 19:58:39 +0100

    Send Money Banner section Completed

--- components/home/hero/Hero3D.tsx
+++ components/home/hero/Hero3D.tsx
@@ -5,7 +5,7 @@ import { CiMoneyBill, CiShare2 } from "react-icons/ci";
 import { GoPeople } from "react-icons/go";
 import { PiUserCirclePlus } from "react-icons/pi";
 import { LuFiles } from "react-icons/lu";
-import LinkSpan from "../LinkSpan";
+import LinkSpan from "../../LinkSpan";
 
 type Hero3DItemsProps = {
   title: string;
@@ -17,7 +17,7 @@ const Hero3DItems = ({ title, children, variant }: Hero3DItemsProps) => {
   return (
     <div className="flex flex-col gap-8 items-center">
       <div className="flex">
-        <span className={`p-6 bg-${variant} rounded-full`}>{children}</span>
+        <span className={`p-6 ${variant === "purple" ? "bg-purple-30" : variant === "blue" ? "bg-blue-500" : variant === "pink" ? "bg-pink-600" : "bg-green-20"} rounded-full`}>{children}</span>
       </div>
       <p className="max-w-[100px] text-xl md:text-sm text-center">{title}</p>
     </div>
@@ -33,7 +33,7 @@ const Hero3D = () => {
           continent.
         </h2>
         <Link href="/">
-          <LinkSpan text="Discover Chipper ID" variant="green-30" />
+          <LinkSpan text="Discover Chipper ID" variant="green" />
         </Link>
 
         <div className="bg-hero-bg bg-contain bg-no-repeat p-6 lg:p-12">
@@ -41,35 +41,35 @@ const Hero3D = () => {
         </div>
 
         <div className="grid grid-cols-3 lg:grid-cols-5 gap-14 lg:gap-32">
-          <Hero3DItems title="Competitive Pricing" variant="green-20">
+          <Hero3DItems title="Competitive Pricing" variant="green">
             <CiMoneyBill
               color="white"
               className="absolute -mt-5 -ml-10"
               size={70}
             />
           </Hero3DItems>
-          <Hero3DItems title="Network Effects" variant="purple-700">
+          <Hero3DItems title="Network Effects" variant="purple">
             <CiShare2
               color="white"
               className="absolute -mt-5 -ml-10"
               size={70}
             />
           </Hero3DItems>
-          <Hero3DItems title="Easy SDK Integration" variant="pink-600">
+          <Hero3DItems title="Easy SDK Integration" variant="pink">
             <LuFiles
               color="white"
               className="absolute -mt-5 -ml-10"
               size={50}
             />
           </Hero3DItems>
-          <Hero3DItems title="Easy for End Users" variant="purple-700">
+          <Hero3DItems title="Easy for End Users" variant="purple">
             <GoPeople
               color="white"
               className="absolute -mt-5 -ml-10"
               size={50}
             />
           </Hero3DItems>
-          <Hero3DItems title="End-to-End KYC" variant="blue-500">
+          <Hero3DItems title="End-to-End KYC" variant="blue">
             <PiUserCirclePlus
               color="white"
               className="absolute -mt-5 -ml-10"
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,15 +14,22 @@ const config: Config = {
           20: "#806ce8",
           30: "#6945d8",
           40: "#d4d6eb",
+          50: "#091644"
         },
         green: {
           20: "#24ddb0",
           30: "#acfde4"
+        },
+        gray: {
+          10: "#f2f2fb"
+        },
+        black: {
+          10: "#1b0a2d"
         }
       },
       backgroundImage: {
-        "hero-bg": "url('/hero3d-bg.png')"
-       
+        "hero-bg": "url('/hero3d-bg.png')",
+        "mobile-ads": "url('/mobile-advert.png')"
       },
       screens: {
         "xs": "440px",
